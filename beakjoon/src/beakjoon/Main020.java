fa689f07683a44f4a729244ac11de6f8eee01593
YeoJeongmo <minawa_55@naver.com>
2024-09-30 13:47:43 +0900

    A+B - 3

---
+++ beakjoon/src/beakjoon/Main020.java
@@ -0,0 +1,17 @@
+package beakjoon;
+
+import java.util.Scanner;
+
+public class Main020 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		int A = 0;
+		int B = 0;
+		for(int i=1;i<=T;i++) {
+			A = sc.nextInt();
+			B = sc.nextInt();
+			System.out.println(A+B);
+		}
+	}
+}
